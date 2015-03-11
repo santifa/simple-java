cf394a276f3fd68a396db5c44c5f651107256635
santifa <juerges.henrik@gmail.com>
2015-03-11 17:07:44 +0100

    7. iteration write ugly code to make the test work

--- simple-tdd/src/test/java/ttd/example/money/MoneyTest.java
+++ simple-tdd/src/test/java/ttd/example/money/MoneyTest.java
@@ -21,4 +21,10 @@ public class MoneyTest {
 		assertFalse(new Dollar(5).equals(new Dollar(4)));
 	}
 
+	@Test
+	public void testFrancMultiplication() {
+		Franc five = new Franc(5);
+		assertEquals(new Franc(10), five.times(2));
+		assertEquals(new Franc(15), five.times(3));
+	}
 }
